04418c3fa41cd7c4bacf79114aa6a593c7290ec8
Pavel Stránský <github@pavelstransky.cz>
2022-03-24 16:57:50 +0100

    Metropolisův algoritmus

--- python/minimize/functions.py
+++ python/minimize/functions.py
@@ -1,6 +1,7 @@
 import random
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import cm           # Colour maps for the contour graph
 
 import minimize
 
@@ -16,15 +17,30 @@ def g(X, parameters=(1, 100)):
     a, b = parameters
     return (a - x)**2 + b * (y - x * x)**2
 
-fnc = g
-path = minimize.minimize(fnc, initial_condition=(2 * random.random() - 1, 2 * random.random() - 1))
+def w(X):
+    x, y = X
+    return x**4 - 2 * x**2 + x + y**2
 
-print("Position of the minimum:", path[-1])
-print(f"Minimum value of function {fnc.__name__}:", fnc(path[-1]))
+def v(X):
+    x, y = X
+    return np.sin(3 * x) * np.sin(3 * y)
 
-# Several trajectories from random position into the minimum
-for i in range(10):
-    path = minimize.minimize(fnc, initial_condition=(2 * random.random() - 1, 2 * random.random() - 1))
+def minimize_and_plot(fnc, box_size=2, num_contours=100, **kwargs):
+    path = minimize.minimize_metropolis(fnc, initial_condition=(0, 0), **kwargs)
+
+    print("Position of the minimum:", path[-1])
+    print(f"Minimum value of function {fnc.__name__}:", fnc(path[-1]))
+
+    x = y = np.linspace(-box_size, box_size, 100)# Range of x and y values for the graph
+
+    X, Y = np.meshgrid(x, y)                     # Grid for calculating values of the function
+    Z = fnc([X, Y])
+
+    plt.contourf(X, Y, Z, num_contours, cmap=cm.hot)
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.colorbar(label=fnc.__name__ + '(x,y)')  # Legend for the contour graph
     plt.plot(path[:,0], path[:,1])
+    plt.show()
 
-plt.show()
+minimize_and_plot(v, temperature=1, max_steps=100000, step_size=0.01)
--- python/minimize/minimize.py
+++ python/minimize/minimize.py
@@ -6,6 +6,18 @@ def random_direction_2d():
     phi = random.uniform(0, 2 * np.pi)
     return np.array([np.cos(phi), np.sin(phi)])
 
+def random_direction_gaussian(dimension):
+    """ Generates random direction in the dimension-dimensional space 
+
+        References:
+        [1] M.E. Muller, A note on a method for generating points uniformly on n-dimensional spheres,
+                Communications of the Asociation for Computing Machinery 2, 19 (1959)
+        [2] G. Marsaglia, Choosing a Point from the Surface of a Sphere,
+                The Annals of Mathematical Statistics 43, 645 (1972)
+    """
+    random_vector_gaussian = np.random.normal(size=dimension)
+    return random_vector_gaussian / np.linalg.norm(random_vector_gaussian)
+    
 def minimize(function, step_size=0.01, initial_condition=(0, 0), max_failed_steps=100):
     """ Simple minimization method using random walk.
 
@@ -43,3 +55,39 @@ def minimize(function, step_size=0.01, initial_condition=(0, 0), max_failed_step
 
     return np.array(path)
 
+def minimize_metropolis(function, dimension=2, temperature=1, step_size=0.01, initial_condition=None, max_steps=20000):
+    """ Simple minimization method using random walk and Metropolis algorithm.
+
+    Arguments:
+    function -- function to minimize
+    dimension -- dimensionality of the function
+    temperature -- temperature of the Metropolis algorithm
+    initial_condition -- the starting point; if None, initial conditions chosen randomly from a box of size given by initial_condition_box
+    max_steps -- total number of steps
+    """
+    if initial_condition is None:       # Random initial condition within box of size given by initialConditionBox
+        initial_condition = np.zeros(dimension)
+    
+    position = np.array(initial_condition)
+    f = function(position)
+
+    path = [position]
+
+    num_steps = 0                    # Total number of steps
+
+    while num_steps < max_steps:
+        new_position = position + step_size * random_direction_gaussian(dimension)
+        newf = function(new_position)
+        C = np.exp((f - newf) / temperature)    # Boltzmann coefficient (for step down C > 1)
+
+        if C > np.random.rand():
+            position = new_position
+            f = newf
+
+            path.append(position)
+
+        num_steps += 1
+
+    print(f"Minimum (Metropolis) = {position}, function value = {f}, steps = {num_steps}")
+
+    return np.array(path)
