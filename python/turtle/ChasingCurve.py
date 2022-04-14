48b7e4ef6145b5108146fce68731c4094f321536
Pavel Stránský <github@pavelstransky.cz>
2022-04-14 16:22:47 +0200

    Stíhací křivka kruh

--- python/turtle/ChasingCurve.py
+++ python/turtle/ChasingCurve.py
@@ -1,19 +1,28 @@
 from turtle import Turtle
 
-def chasing_curve_straight(hunter_velocity=2, dog_velocity=5, dt=1):
+def init_hunter(x=-400, y=300):
     hunter = Turtle()
     hunter.penup()
-    hunter.setposition(-400, 300)
+    hunter.setposition(x, y)
     hunter.color("green")
     hunter.pendown()
 
     hunter.setheading(0)
 
+    return hunter
+
+def init_dog(x=-400, y=-300):
     dog = Turtle()
     dog.penup()
     dog.setposition(-400, -300)
     dog.color("red")
     dog.pendown()
+
+    return dog
+
+def chasing_curve_straight(hunter_velocity=2, dog_velocity=5, dt=1):
+    hunter = init_hunter()
+    dog = init_dog()
 
     while hunter.distance(dog) > dog_velocity * dt:
         hunter.forward(hunter_velocity * dt)
@@ -22,4 +31,17 @@ def chasing_curve_straight(hunter_velocity=2, dog_velocity=5, dt=1):
 
     input()
 
-chasing_curve_straight()
+def chasing_curve_circle(hunter_velocity=2, dog_velocity=5, hunter_turn_velocity=1, dt=1):
+    hunter = init_hunter(x=0)
+    dog = init_dog()
+
+    while hunter.distance(dog) > dog_velocity * dt:
+        hunter.forward(hunter_velocity * dt)
+        hunter.right(hunter_turn_velocity * dt)
+        dog.setheading(dog.towards(hunter))
+        dog.forward(dog_velocity * dt)
+
+    input()
+
+#chasing_curve_straight()
+chasing_curve_circle(hunter_velocity=8, hunter_turn_velocity=2)
