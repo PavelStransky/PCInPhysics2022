f6cc637daf9314d24c8892847e68323f1286977b
Pavel Stránský <github@pavelstransky.cz>
2022-04-30 06:19:56 +0200

    Monte Carlo - srdce

--- montecarlo/integration.py
+++ montecarlo/integration.py
@@ -27,6 +27,31 @@ def integrateh(n=10000):
 
     print(f"I3 = {result} (number of hits: {hits}, volume of the integration region: {volume})")
 
-print(integrate(f, 0, 2*np.pi, n=100000000))
-print(integrate(g, 0, np.sqrt(10*np.pi), n=100000000))
-integrateh(n=1000000)
+def heart(phi):
+    return np.sin(phi)**3, (13 * np.cos(phi) - 5 * np.cos(2 * phi) -2 * np.cos(3 * phi) - np.cos(4 * phi)) / 16
+
+def integrateHeart(n=10000):
+    hits = 0
+    result = 0
+
+    bounds = 1.1
+
+    for _ in range(n):
+        x, y = np.random.uniform(-bounds, bounds, 2)
+
+        phi = np.arctan2(y, x)
+        bx, by = heart(phi)
+        if x*x + y*y < bx*bx + by*by:
+            hits += 1
+            result += np.exp((x**2 + y**2) / 2)
+
+    volume = (2 * bounds)**2 * hits / n
+    result = result / hits * volume / (2 * np.pi)
+
+    print(f"Iheart = {result} (number of hits: {hits}, volume of the integration region: {volume})")
+
+#print(integrate(f, 0, 2*np.pi, n=100000000))
+#print(integrate(g, 0, np.sqrt(10*np.pi), n=100000000))
+#integrateh(n=1000000)
+
+#integrateHeart(1000000)
